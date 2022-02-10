5a3570c485e819deceac5ce7ebb13d6a6cf9053e
adrianmonaghan <adrianmonaghan20@gmail.com>
2022-02-10 13:16:19 -0500

    Getting there

--- main.js
+++ main.js
@@ -9,6 +9,24 @@ Modified: 12/06/21
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
+// function button1Clicked() {
+//     console.log("Button 1 was clicked"); 
+//     let randNum = Math.random();
+//     console.log(randNum);
+//     return randNum;
+// }
+
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let randNum = Math.random();
+    let newText = "Button was Clicked";
+
+    console.log(randNum);
+    //return randNum;
+
+    let button = document.getElementById("random_num");
+    button.innerHTML = randNum;
 }
+
+console.log("Hello world");
+
+//document.getElementById("btn1").onclick = button1Clicked;
